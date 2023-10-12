ce9015352e3369fd9a5f7e6cd101d83d51ed99da
NathanUlmen <Ulmennathan@gmail.com>
2023-10-12 11:04:39 -0600

    initial commit

---
+++ DriverClass.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DriverClass {
+    public static void main(String[] args) {
+        MainClass printer = new MainClass();
+        int userInput;
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter an integer dividend to calculate: ");
+        userInput = keyboard.nextInt();
+
+        printer.doIt(userInput);
+    }
+}
